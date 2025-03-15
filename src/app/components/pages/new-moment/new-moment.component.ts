25b9ea221a790ad3c36ea073474ae39d4fa59d1e
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-15 12:11:34 -0300

    style: add style in new moment component

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent {
+  constructor() {}
+  ngOnInit(): void {}
 
+  btnText = 'Compartilhar';
 }
