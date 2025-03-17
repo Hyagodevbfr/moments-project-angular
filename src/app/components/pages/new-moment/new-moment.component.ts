da47e9ac1660f2a47329c1049858ad12d83c70d7
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-17 15:08:22 -0300

    feat: add service api in new moment

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,7 @@
+import { MomentService } from './../../../services/moment.service';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Moment } from 'src/app/interfaces/moment';
 
 @Component({
   selector: 'app-new-moment',
@@ -6,8 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent {
-  constructor() {}
+  constructor(private momentService: MomentService) {}
   ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
 
   btnText = 'Compartilhar';
 }
