dbc4ab15f650561e92c4fef4608374148dc52621
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-20 13:56:57 -0300

    feat: implements comments function

--- src/app/services/coment.service.ts
+++ src/app/services/coment.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+import { environment } from 'environment/environment';
+import { Comment } from '../interfaces/comment';
+import { Response } from '../interfaces/response';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  createComment(data: Comment): Observable<Response<Comment>> {
+    const url = `${this.apiUrl}/${data.momentId}/comments`;
+    return this.httpClient.post<Response<Comment>>(url, data);
+  }
 }
